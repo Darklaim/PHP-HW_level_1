850786651d2daf6986821161dc3e925c2f07a0fe
Darklaim <Darklaim@gmail.com>
2018-11-05 15:22:20 +0300

    Corrected Task_4

--- Lesson_2/Task_4.php
+++ Lesson_2/Task_4.php
@@ -8,27 +8,42 @@
 
 $arg1 = rand (0, 10);
 $arg2 = rand (0, 10);
-mathOperation($arg1, $arg2, 'mult');
+mathOperation($arg1, $arg2,'sum');
+mathOperation($arg1, $arg2, 'dif');
+mathOperation($arg1, $arg2,'mult');
+mathOperation($arg1, $arg2, 'div');
 function mathOperation($arg1, $arg2, $operation){
     switch ($operation){
         case 'sum': {
-            $sum = $arg1 + $arg2;
+            function sum($arg1, $arg2){
+                return $arg1 + $arg2;
+            }
+            $sum = sum($arg1, $arg2);
             echo $arg1.' + '.$arg2.' = '.$sum.'<br>';
             break;
         }
         case 'dif': {
-            $dif = $arg1 - $arg2;
+            function dif($arg1, $arg2){
+            return $arg1 - $arg2;
+            }
+            $dif = dif($arg1, $arg2);
             echo $arg1.' - '.$arg2.' = '. $dif.'<br>';
             break;
         }
         case 'mult': {
-            $mult = $arg1 * $arg2;
+            function mult($arg1, $arg2){
+                return $arg1 * $arg2;
+            }
+            $mult = mult($arg1, $arg2);
             echo $arg1.' * '.$arg2.' = '.$mult.'<br>';
             break;
         }
         case 'div': {
-            if ($arg2 == 0) break;
-            $div = $arg1 / $arg2;
+            function div($arg1, $arg2){
+                if ($arg2 == 0) return 'На 0 делить нельзя';
+                return $arg1 / $arg2;
+            }
+            $div = div($arg1, $arg2);
             echo $arg1.' / '.$arg2.' = '.$div.'<br>';
         }
     }

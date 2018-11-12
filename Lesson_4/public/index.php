5a96921614e1bf7f4a094b1511804c838f6bf445
Darklaim <Darklaim@gmail.com>
2018-11-12 15:51:24 +0300

    Lesson_4

---
+++ Lesson_4/public/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../config/config.php');
+renderPage();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 5</title>
+</head>
+<body>
+
+<div class="slider">
+  <?php
+
+  ?>
+
+</body>
+</html>

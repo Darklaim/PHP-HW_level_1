ade061e886dadcb67cce9b56e2a50f6778371305
Darklaim <Darklaim@gmail.com>
2018-11-14 20:16:00 +0300

    Lesson_4

--- Lesson_4/public/index.php
+++ Lesson_4/public/index.php
@@ -1,20 +1,35 @@
-<?php
-require_once('../config/config.php');
-renderPage();
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Lesson 5</title>
+    <title>Lesson 4</title>
 </head>
 <body>
 
 <div class="slider">
-  <?php
-
-  ?>
+<?php
+// Функция для удаления лишних файлов 
+function excess($files) {
+  $result = array();
+  for ($i = 0; $i < count($files); $i++) {
+    if ($files[$i] != "." && $files[$i] != "..") $result[] = $files[$i];
+  }
+  return $result;
+}
+  $dir = "img"; // Путь к директории, в которой лежат изображения
+  $files = scandir($dir); // Получаем список файлов из этой директории
+  $files = excess($files); // Удаляем лишние файлы
+?>
+<?php
+// Вывод изображений на страницу сайта по 3 штуки на одну строку
+for ($i = 0; $i < count($files); $i++) { ?>
+<a href="<?=$dir."/".$files[$i]?>" target="_blank"><img src="<?=$dir."/".$files[$i]?>" alt="picrute" width = 200 height = 250 /></a>
+<?php if (($i + 1) % 3 == 0) { 
+echo '<br>';
+  } 
+} 
+?>
+</div>
 
 </body>
 </html>
